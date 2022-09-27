bd2a6f128cdf262ce4cd263c5da70b755dc7f735
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2022-09-27 08:33:22 +0700

    initial

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\User;
+use Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Default credentials
+        \App\Models\User::insert([
+            [ 
+                'name' => 'Left4code',
+                'email' => 'admin@pln.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('admin123'),
+                'active' => 1,
+                'remember_token' => Str::random(10)
+            ]
+        ]);
+
+        // Fake users
+        User::factory()->times(9)->create();
+    }
+}
